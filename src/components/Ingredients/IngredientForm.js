90fac04b4d761c5d3804b0e5d346acef75fe5bcc
Imre Bartis <imre.bartis@siili.com>
2020-01-05 08:34:40 +0100

    array destructuring

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -4,7 +4,7 @@ import Card from "../UI/Card";
 import "./IngredientForm.css";
 
 const IngredientForm = React.memo(props => {
-  const inputState = useState({ title: "", amount: "" });
+  const [inputState, setInputState] = useState({ title: "", amount: "" });
   const submitHandler = event => {
     event.preventDefault();
     // ...
@@ -19,10 +19,10 @@ const IngredientForm = React.memo(props => {
             <input
               type="text"
               id="title"
-              value={inputState[0].title}
+              value={inputState.title}
               onChange={event => {
                 const newTitle = event.target.value;
-                inputState[1](prevInputState => ({
+                setInputState(prevInputState => ({
                   title: newTitle,
                   amount: inputState.amount
                 }));
@@ -32,10 +32,10 @@ const IngredientForm = React.memo(props => {
           <div className="form-control">
             <label
               htmlFor="amount"
-              value={inputState[0].amount}
+              value={inputState.amount}
               onChange={event => {
                 const newAmount = event.target.value;
-                inputState[1](prevInputState => ({
+                setInputState(prevInputState => ({
                   amount: newAmount,
                   title: inputState.title
                 }));
